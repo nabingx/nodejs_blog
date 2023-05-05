0f0e320a1bf233a0758b7d368c68825194cca2c2
NGUYEN NGOC ANH 20194476 <nngocanh697@gmail.com>
2023-05-05 22:05:18 +0700

    Fix2Run husky

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // Template engine
 app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
-        app.set('view engine', '.hbs');
+                app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
